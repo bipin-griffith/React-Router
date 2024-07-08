b93ec2ac57879dc83d0bc695c5d0ec8d31730272
Bipin Gupta <bgupta9861@gmail.com>
2024-07-08 02:03:26 +0100

    added product page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Product from "./pages/Product";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }, {}]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/product", element: <Product /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
